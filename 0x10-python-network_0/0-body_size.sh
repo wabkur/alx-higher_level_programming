7688caea914ff8d1f7a9c4b8d5b57d4e50714954
wabkur <wabilitukur.com>
2023-03-30 22:59:07 +0100

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,9 +3,4 @@
 # Get the URL from the command line argument
 url=$1
 
-# Send a request to the URL and get the size of the response body
-size=$(curl -s $url | wc -c)
-
-# Display the size of the response body
-echo "The size of the response body is: $size bytes"
-
+curl -s "$1" | wc -c
