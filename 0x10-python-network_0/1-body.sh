9322197ea335fe5992be6f4e3aa156cf7fdda4a4
wabkur <wabilitukur.com>
2023-03-30 23:35:20 +0100

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Script that shows the response body redirecting /route_1
-curl -Ls "$1"
-#!/bin/bash
 # send a GET request to an URL with curl, and display the body of the response
 curl -sL "$1"
