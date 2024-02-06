2a81817726beeb23bb649aa243ec89435222a914
wabkur <wabilitukur@gmail.com>
2024-02-06 04:05:07 -0800

    safe

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    
+    try:
+        besh = a / b
+    except ZeroDivisionError:
+        besh = None
+    finally:
+        print("Inside result:", "{}".format(besh))
+        return besh
