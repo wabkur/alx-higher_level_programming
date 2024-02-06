d001afeb726191163b4c7658aade2422189c08f7
wabkur <wabilitukur@gmail.com>
2024-02-06 02:12:43 -0800

    safe

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    y = 0
+    besh = 0
     for i in range(x):
         try:
             print("{:d}".format(my_lists[i]), end="")
-            y += 1
+            besh += 1
         except (ValueError, TypeError):
             pass
-        print()
-        return (y)
+        print("")
+        return (besh)
 
