423f1b44f6a37667ed137590ca92601a854ba8ca
wabkur <wabilitukur@gmail.com>
2024-02-11 00:58:47 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args = sys.argv
-    num = len(args) - 1
+    num = len(sys.argv) - 1
     if num == 0:
         print("0 arguments:")
     elif num == 1:
