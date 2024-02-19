9113071e8511fa91e07ec1a6564487beca4ee8e2
wabkur <wabilitukur@gmail.com>
2024-02-19 05:29:14 -0800

    kind

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,4 +3,4 @@
 
 def is_kind_of_class(obj, a_class):
     """True if obj is an instance or inherited from a_class, else false """
-    return isintance(obj, a_class)
+    return isinstance(obj, a_class)
