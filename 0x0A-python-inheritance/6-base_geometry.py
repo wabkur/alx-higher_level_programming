a9b655eb9275ac189567127b58cd32fbe3197b8f
wabkur <wabilitukur@gmail.com>
2024-02-19 06:53:50 -0800

    geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Raise an exception for non implementation of class BaseGeometry"""
+
+class BaseGeometry:
+    """Raise exception with message."""
+    def area(self):
+        raise Exception("area() is not implemented")
