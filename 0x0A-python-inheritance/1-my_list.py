543c610c7ded1188f8a28157cdaa7fffba420fe2
wabkur <wabilitukur@gmail.com>
2024-02-20 04:07:05 -0800

    list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""contain class that inherits"""
+
+
+class MyList(list):
+    """Contain subclass of list"""
+    def __init__(self):
+        super().__init__()
+
+        def print_sorted(self):
+            """print the sorted list"""
+            print(sorted(self))
