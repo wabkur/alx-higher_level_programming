ba9c287dc0d899a564504b15aea85950bb995a1d
wabkur <wabilitukur@gmail.com>
2024-02-20 01:59:06 -0800

    kind

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """contain the first go at it instance"""
 
+
 def is_kind_of_class(obj, a_class):
     """True if obj is an instance or inherited from a_class, else false """
     return isinstance(obj, a_class)
