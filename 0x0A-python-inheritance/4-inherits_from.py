24cfb154292256cc1fa48e0af10344c7eeac068a
wabkur <wabilitukur@gmail.com>
2024-02-20 02:09:17 -0800

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """contain the inherits_from function that checks for only subclass"""
+
+
 def inherits_from(obj, a_class):
     """Return True if obj is a subclass of a_class, otherwise false"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if is subclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
