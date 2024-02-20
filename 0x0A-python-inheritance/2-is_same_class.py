8744068ef98bd89eafaeb5a55161ca490c53c6aa
wabkur <wabilitukur@gmail.com>
2024-02-20 01:55:23 -0800

    class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """check if obj is exactly an instance of the class"""
+
+
 def is_same_class(obj, a_class):
     """If object is exactly an instance of a_class - True.
     otherwise - False.
