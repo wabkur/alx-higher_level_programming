d515169c39b626719504540eda92c32e0d0e11e6
wabkur <wabilitukur@gmail.com>
2024-03-09 00:32:55 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    w = len(sys.argv) - 1
 
-    num = len(sys.argv) - 1
-     if num == 0:
-        print("0 arguments:")
-    elif num == 1:
-        print("1 argurment:")
+    if w == 0:
+        print("{} arguments.".format(w))
+    elif w == 1:
+        print("{} argument:".format(w))
     else:
-        print("{} argurments:".format(num))
-        for i in range(num):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(w))
+
+    if w >= 1:
+        w = 0
+        for arg in sys.argv:
+            if w != 0:
+                print("{}: {}".format(w, arg))
+            w += 1
