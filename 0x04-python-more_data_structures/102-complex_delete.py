962c2f840a20f68dafbe257de69654b18f65b4a6
wabkur <wabilitukur@gmail.com>
2024-03-09 07:32:59 -0800

    compl

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    get = []
+    if value in a_dictionary.values():
+        for days, dic in a_dictionary.items():
+            if value == dic:
+                get.append(key)
+
+        for kool in keylist:
+            if kool in a_dictionary:
+                del a_dictionary[kl]
+    return a_dictionary
