d44e02d9016e2ed0d3964ab4bde679e1fcb868bf
wabkur <wabilitukur@gmail.com>
2024-03-09 04:19:22 -0800

    element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx = -1:
+        return (None)
+
+    besh = len(my_list)
+
+    if idx > besh - 1:
+        return (None)
+
+    return(my_list[idx])
