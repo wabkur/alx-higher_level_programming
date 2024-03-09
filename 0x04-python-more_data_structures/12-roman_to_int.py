e663094e6bf1f2ab3df6d9b2c7c5d48bbde989fb
wabkur <wabilitukur@gmail.com>
2024-03-09 07:16:19 -0800

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_map = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+    get = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
             }
+
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     sets, sum = 0, 0
 
-    for i in roman_string:
-        sum += roman_map[i] if roman_map[i] <= stes else roman_map[i] - sets * 2
-        sets = roman_map[i]
+    for r in roman_string:
+        sum += get[r] if get[r] <= sets else get[r] - sets * 2
+        sets = get[r]
     return sum
