85d4368aa6744fd6a51424e0cfcd6f3f7b9ea8e3
wabkur <wabilitukur@gmail.com>
2024-03-10 00:13:42 -0800

    sqr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent the square."""
+
+    def __init__(get, size):
+        """Initialize a new Square.
+        Args:
+            size (int): A size of new square.
+        """
+        get.__size = 
