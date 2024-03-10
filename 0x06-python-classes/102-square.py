0c742ef4e5bd9665cea66e81bc006a040f91f250
wabkur <wabilitukur@gmail.com>
2024-03-10 01:33:10 -0800

    py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square that defines a square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int and type(value) is not float:
+            raise TypeError('size must be a number')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value  #: size of the square
+
+    def area(self):
+        return self.__size**2
+
+    def __lt__(self, other):
+        return self.size < other.size
+
+    def __le__(self, other):
+        return self.size <= other.size
+
+    def __eq__(self, other):
+        return self.size == other.size
+
+    def __ne__(self, other):
+        return self.size != other.size
+
+    def __ge__(self, other):
+        return self.size >= other.size
