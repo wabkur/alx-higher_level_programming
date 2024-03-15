cd13bca33931ffe0f3bd01a6b0a831dd19427ac7
wabkur <wabilitukur@gmail.com>
2024-03-15 08:00:21 -0700

    py

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_python_bytes - print some basic bytes information
+ * @p: python object
+ * Return: No return
+ **/
+void print_python_bytes(PyObject *p)
+{
+}
+/**
+ * print_python_float - print Python float information
+ * @p: python object
+ * Return: No return
+ **/
+void print_python_float(PyObject *p)
+{
+}
+/**
+ * print_python_list - print some basic list information
+ * @p: python object
+ * Return: No return
+ **/
+void print_python_list(PyObject *p)
+{
+}
