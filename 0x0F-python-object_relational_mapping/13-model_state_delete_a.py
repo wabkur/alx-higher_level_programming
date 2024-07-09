7a9ac658de05b5c7513a064448823c05b6c30a44
wabkur <wabilitukur@gmail.com>
2024-07-09 03:14:00 -0700

    ORM

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that prints the State object with the name passed as argument from the database
+""" Prints the State object with the name passed as argument from the database
 """
 import sys
 from model_state import Base, State
