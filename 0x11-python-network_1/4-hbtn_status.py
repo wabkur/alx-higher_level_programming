de3bb51edf167e35ad0d590b493b333a43fd518c
wabkur <wabilitukur@gmail.com>
2024-07-22 03:20:30 -0700

    network

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+if __name__ == '__main__':
+    import requests
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
